fd8c62b02fcbbb5535836ff70ef567b3e32dadcc
YiJunGao <1141180618@qq.com>
2019-12-14 11:41:52 +0800

    spring boot 创建定时任务

---
+++ src/main/java/com/example/schedulerdemo/ScheduledTasks.java
@@ -0,0 +1,27 @@
+package com.example.schedulerdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+
+    /*
+        您可以通过使用批注中的fixedRate参数来安排以固定间隔执行的方法@Scheduled。
+        在下面的示例中，带注释的方法将每2秒执行一次。
+     */
+    @Scheduled(fixedRate = 2000)
+    public void scheduleTaskWithFixedRate() {
+        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
+    }
+
+}
